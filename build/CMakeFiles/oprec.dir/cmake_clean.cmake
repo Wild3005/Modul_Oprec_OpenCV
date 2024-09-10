38b2483d625a6a0497c53936f9a9c34f7a60c28b
Wild3005 <wildanrisqi04@gmail.com>
2024-09-10 20:00:00 +0700

    modul opencv

---
+++ build/CMakeFiles/oprec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oprec.dir/src/opencv_4_color_detect.cpp.o"
+  "oprec"
+  "oprec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oprec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
